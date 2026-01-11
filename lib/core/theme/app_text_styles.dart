d693046b7d5d0b92e5e090ad2acd47e94fe7941d
Vishwajeet <vishwajeetadkine705@gmail.com>
2026-01-11 14:35:23 +0530

    Create app_text_styles.dart

---
+++ lib/core/theme/app_text_styles.dart
@@ -0,0 +1,66 @@
+class AppTextStyles {
+  // Headings
+  static const h1 = TextStyle(
+    fontSize: 24,
+    fontWeight: FontWeight.bold,
+    color: AppColors.white,
+  );
+  
+  static const h2 = TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.bold,
+    color: AppColors.white,
+  );
+  
+  static const h3 = TextStyle(
+    fontSize: 18,
+    fontWeight: FontWeight.bold,
+    color: AppColors.white,
+  );
+  
+  static const h4 = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.bold,
+    color: AppColors.white,
+  );
+  
+  // Body Text
+  static const body1 = TextStyle(
+    fontSize: 16,
+    color: AppColors.white,
+  );
+  
+  static const body2 = TextStyle(
+    fontSize: 14,
+    color: AppColors.white,
+  );
+  
+  static const body3 = TextStyle(
+    fontSize: 12,
+    color: AppColors.white,
+  );
+  
+  // Subtitle
+  static const subtitle1 = TextStyle(
+    fontSize: 14,
+    color: AppColors.textGray,
+  );
+  
+  static const subtitle2 = TextStyle(
+    fontSize: 12,
+    color: AppColors.textGray,
+  );
+  
+  // Special
+  static const button = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.w600,
+    color: AppColors.white,
+  );
+  
+  static const caption = TextStyle(
+    fontSize: 11,
+    fontWeight: FontWeight.w600,
+    color: AppColors.primary,
+  );
+}
